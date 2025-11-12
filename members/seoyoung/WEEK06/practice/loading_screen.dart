c357a3b7c160b1421a6b8a3728fc05d1cc9bf573
Seoyoung Hong <applesoha@gmail.com>
2025-11-12 13:30:40 +0900

    Add WEEK06 assignment files for seoyoung

--- members/seoyoung/WEEK06/practice/loading_screen.dart
+++ members/seoyoung/WEEK06/practice/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
+import 'location.dart';
 import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
--- members/seoyoung/WEEK06/practice/main.dart
+++ members/seoyoung/WEEK06/practice/main.dart
@@ -1,4 +1,4 @@
-import 'package:clima/screens/loading_screen.dart';
+import 'loading_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
